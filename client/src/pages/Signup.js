79c530cd71e6a09fe34200a9e237670299d002d6
Your Name <you@example.com>
2025-04-23 01:52:23 +0530

    lele

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -22,7 +22,7 @@ const SignupPage = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post("https://quiz-application-main.onrender.com", formData);
+      const res = await axios.post("http://localhost:5000/api/auth/signup", formData);
       alert(res.data.message); // Show success message
       navigate("/login"); // Redirect to login
     } catch (err) {
