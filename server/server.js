277673a951bf24a74ae9232fb21b1eb362027862
Your Name <you@example.com>
2025-04-23 08:50:03 +0530

    ho jaa

--- server/server.js
+++ server/server.js
@@ -39,6 +39,11 @@ mongoose.connect(process.env.MONGO_URI)
 app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api/quiz", require("./routes/quizRoutes")); // ✅ VERY IMPORTANT
 app.use("/api/admin", require("./routes/adminRoutes"));
-
+app.get("/", (req, res) => {
+  res.send({
+    activeStatus: true,
+    message: "Server is active"
+  });
+});
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
