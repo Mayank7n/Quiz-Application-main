aae8070dc7b70f60cf2b00feb857b77255d05921
Your Name <you@example.com>
2025-04-23 10:32:20 +0530

    final 1

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -22,7 +22,7 @@ const SignupPage = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post("https://quiz-application-main-5njxup0m1-mayank7ns-projects.vercel.app/api/auth/signup", formData);
+      const res = await axios.post("https://quiz-application-main-alpha.vercel.app/api/auth/signup", formData);
       alert(res.data.message); // Show success message
       navigate("/login"); // Redirect to login
     } catch (err) {
